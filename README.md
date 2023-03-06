39fcc6839e2ea9eb6269ef4c5c5a7115cb1acdcd
sherphy <sherlynn.phyllisa@u.nus.edu>
2023-03-07 03:37:53 +0800

    header done

--- README.md
+++ README.md
@@ -25,6 +25,10 @@ I believe it's something to do with my webpack configuration, but I'm unable to 
 You can refer to App.css - I believe my @font-face code should be right
 
 DISCREPENCIES: 
-1. For the banner header, our photos have inverted resizing - probably because I designed mobile-first instead of desktop-first (I used object-fill: cover; width: 100%; height: auto; for phone view, while desktop media query displays max-height: 30vh) 
+1. For the banner header, our photos have inverted resizing - probably because I designed mobile-first instead of desktop-first (I used object-fill: cover; width: 100%; height: auto; for phone view, while desktop media query displays max-height: 30vh). Same inversion goes for menu overlay height
+2. The cross logo to exit out of menu overlay: I used float, I think you guys used flex-end instead 
+
+IMPROVEMENTS:
+1. I want to clamp the menu overlay height. I want it to be clamp(minimum-banner,33vh,maximum-content) in case the sentences overflow. 
 
 sorry if some naming conventions are weird 
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,24 +1,46 @@
 import React from "react";
+import { useState } from "react";
 import "./Banner.css";
 import bannerPhoto from "../assets/banner.png";
 import bannerLogo from "../assets/bannerLogo.svg";
 import companyLogo from "../assets/companyLogo.png";
-import menu from "../assets/menu.png";
+import menuLogo from "../assets/menu.png";
+import escapeLogo from "../assets/escapeLogo.png";
 
 const Banner = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
+    <>
     <div className="bannerContainer">
       <div className="banner">
         <img src={bannerPhoto} id="banner-photo" />
         <img src={companyLogo} id="company-logo" /> 
-        <img src={menu} id="menu" /> 
+        <img src={menuLogo} id="menu-logo" onClick={toggleMenu}/> 
         <div id="banner-words-center">
-          <img src={bannerLogo} id="banner-logo" /> <br />
+          <img src={bannerLogo} id="banner-logo"/> <br />
           <h1 id="banner-headline">STORIES OF FREEDOM</h1>
         </div>
+        {/* if menu overlay is clicked  */}
+        {menuOpen && 
+          <div className="menu"> 
+          <img src={escapeLogo} height='50px' id="escape-logo" onClick={toggleMenu}/>
+            <div className="menu-titles">
+              <p>Harley-Davidson riders on how motorcycles helped them find their purpose</p>
+              <p>I'm the only woman in my group and at times, I also lead the group</p>
+              <p>Riding is a family affair</p>
+              <p>More stories of freedom</p>
+            </div>
+          </div>
+        }
       </div>
       <span id="banner-bottom"></span>
     </div>
+    </>
   );
 };
 
